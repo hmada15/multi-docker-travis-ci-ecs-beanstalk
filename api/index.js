8a3df47a2978606c484df57fd9afef72a66f579c
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-08-01 16:12:52 +0200

    Add api logic

---
+++ api/index.js
@@ -0,0 +1,66 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.postgresUser,
+  host: keys.postgresHost,
+  database: keys.postgresDatabase,
+  password: keys.postgresPassword,
+  port: keys.postgresPort,
+});
+
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+});
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  redisClient.hset('values', index, 'Not calculated yet');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log('It wrok');
+});
---
+++ api/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  postgresUser: process.env.POSTGRES_USER,
+  postgresHost: process.env.POSTGRES_HOST,
+  postgresDatabase: process.env.POSTGRES_DATABASE,
+  postgresPassword: process.env.POSTGRES_PASSWORD,
+  postgresPort: process.env.POSTGRES_PORT,
+};
